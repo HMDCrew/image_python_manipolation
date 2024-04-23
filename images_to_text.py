3c4764206943e724862ecdf5628239efa0337746
Andrei Leca <HMDCrew@users.noreply.github.com>
2024-04-23 11:11:22 +0200

    text finder true direction

--- images_to_text.py
+++ images_to_text.py
@@ -9,20 +9,22 @@ img = cv2.imread("1.tif", cv2.IMREAD_GRAYSCALE)
 img_w, img_h = img_helper_dimension(img)
 img = resize(cv2, img, img_w, img_h)
 
-# Binarizza l'immagine per separare i punti neri di aggregazione
-_, binary_image = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+def find_black_points(gray_image):
 
-# Trova i contorni nell'immagine binarizzata
-contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    _, binary_image = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
 
-# Itera sui contorni trovati
-for contour in contours:
-    # Calcola il rettangolo del contorno
-    x, y, w, h = cv2.boundingRect(contour)
-    
-    # Disegna il rettangolo rosso intorno al punto nero di aggregazione
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    # Trova i contorni neri
+    #contours, _ = cv2.findContours(binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_EXACT)
+    #contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(binary_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-show_img(cv2, img)
+    # Disegna cerchi rossi sui punti neri
+    for contour in contours:
+        for point in contour:
+            cv2.circle(gray_image, (point[0][0], point[0][1]), 3, (0, 0, 255), -1)
+
+    return gray_image
+
+show_img(cv2, find_black_points(img))
 
 cv2.waitKey(0)
